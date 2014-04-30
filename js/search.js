8c48c95514982b44ade5f7e9f9a56ddc5e3a858c
Sharada <sharada>
2014-04-30 15:47:47 -0400

    ap.html results list displaying correctly below map

--- js/search.js
+++ js/search.js
@@ -3,4 +3,13 @@ function init()
 	console.log("start");
 //	var response = https://api.locu.com/v1_0/menu_item/search/?name=latte&api_key=e18c6ceaae14397336d3a768afc41d7fbbd6bbca;
 //	console.log(response);
+
+	var locu = require('locu');
+	var mclient = locu.MenuItemClient(e18c6ceaae14397336d3a768afc41d7fbbd6bbca);
+	mclient.search({name:'espresso', locality:'San Francisco'},
+					function(result){
+						console.log(result.objects[0]);
+					});
+
+	document.write(mclient);
 }
