77cfd10bb8220614db548cc8cd8e6c6602787b6e
Sharada <sharada>
2014-04-29 22:55:47 -0400

    adding search.html which uses Locu api

---
+++ js/search.js
@@ -0,0 +1,6 @@
+function init()
+{
+	console.log("start");
+//	var response = https://api.locu.com/v1_0/menu_item/search/?name=latte&api_key=e18c6ceaae14397336d3a768afc41d7fbbd6bbca;
+//	console.log(response);
+}
